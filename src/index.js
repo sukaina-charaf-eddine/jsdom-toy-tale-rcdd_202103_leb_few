305a41fa7eef3e892cdcf14bca1fbda92721c20e
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-26 08:16:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+const addBtn = document.querySelector('#new-toy-btn');
+const toyForm = document.querySelector('.container');
+const toyCollection = document.querySelector('#toy-collection');
 let addToy = false;
+const createToyForm = document.querySelector('.add-toy-form');
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
@@ -14,11 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-const toyCollection = document.querySelector('#toy-collection')
-let addToy = false
-const createToyForm = document.querySelector('.add-toy-form')
+
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -30,21 +30,18 @@ addBtn.addEventListener('click', () => {
   }
 })
 
-// if the <script> is at the top of the page, can use
-// document.addEventListener('DOMContentLoaded', () => {})
-// code goes here!
 
 const initialize = () => {
   fetch('http://localhost:3000/toys')
     .then(resp => resp.json())
-    // divide toys here
+    
     .then(toys => toys.forEach(showToy))
 }
 
 const showToy = toy => {
   let thisToy = toy
   let toyDiv = document.createElement('div')
-  // add class to class list array
+  
   toyDiv.classList.add('card')
   toyDiv.innerHTML = `
     <h2>${toy.name}</h2>
